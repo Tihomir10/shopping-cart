55f46f603bc2659efdcfa8042c3bcc270903c058
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-18 16:35:10 +0100

    adding products to cart

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,42 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Homepage from './components/Homepage';
 import Shop from './components/shop/Shop';
 import NavBar from './components/Navbar';
 import Cart from './components/Cart';
-import Product from './components/shop/Product'
+import Product from './components/shop/Product';
+import { array } from './components/assests/assets'
 import './index';
 
 function Routes() {
+
+  const [ cart, setCart ] = useState([]);
+
+  function addItem(event)  {
+    const item = array.filter(obj => {
+      return obj.id == event.target.id;
+    })
+    var cartItem = item[0]
+    
+    for(var i = 0; i < cart.length; i++) {
+      if (cart[i].name == cartItem.name) {
+        setCart(
+          cart.map(
+            (item) => (item.name === cartItem.name ? { ...item, qty: item.qty + 1 } : item)
+          )
+        );
+        return
+      }
+    }
+
+    setCart([...cart, {
+      qty: 1,
+      name: cartItem.name,
+      price: cartItem.price
+    }])
+  }
+  
   return (
     <BrowserRouter>
       <NavBar />
@@ -18,10 +47,13 @@ function Routes() {
         <Route exact path='/shop'>
           <Shop />
         </Route>
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
-        <Route path='/shop/:name' component={Product} />
+        <Route exact path='/cart' component={Cart} />
+        <Route 
+          path='/shop/:name'  
+          render={(props) => (
+            <Product {...props} addItem={addItem} />
+          )}
+        />
       </Switch>
     </BrowserRouter>
   )
--- src/components/shop/Product.js
+++ src/components/shop/Product.js
@@ -1,13 +1,10 @@
-
 import { array } from '../assests/assets';
 
-function Product({match}) {
+function Product(props) {
   
   const item = array.filter(obj => {
-    return obj.name == match.params.name
+    return obj.name == props.match.params.name
   })
-
-  console.log(item[0])
 
   return (
     <div id='product'>
@@ -17,7 +14,8 @@ function Product({match}) {
       <div id='product-info'>
         <h3>{item[0].name}</h3>
         <h5>{item[0].price}$</h5>
-        <button className='btn btn-primary'>Add to Cart</button>
+        <button id={item[0].id} className='btn btn-primary' onClick={props.addItem}>Add to Cart</button>
+        <button className='btn btn-primary'>Copmplete Order</button>
       </div>
     </div>
   )
